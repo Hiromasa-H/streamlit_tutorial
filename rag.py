b4a3e160dc76627268fa25ff5235fbd23253db89
Hiromasa-H <hhiromasa217@gmail.com>
2024-10-19 03:20:56 +0900

    added prompt formatting and model invoking

--- rag.py
+++ rag.py
@@ -2,8 +2,10 @@ from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoad
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
 import shutil
 import os
+from langchain_openai import ChatOpenAI
 
 def load_PDF(path: str) -> list:
     if os.path.isdir(path):
@@ -43,7 +45,36 @@ def save_chunks_to_database(chunks: list,db_path: str):
     
     return vector_db
 
+def get_context_from_db(vector_db, query):
+    # results = vector_db.similarity_search_with_relevance_score(query,k=3)
+    # context = '\n\n--\n\n'.join([doc.page_content for doc, _score in results])
+    # return context
+    retriever = vector_db.as_retriever()
+    context = retriever.invoke(query)
+    return context
+
+PROMPT = """
+Answer the following questions based on the given context:
+{CONTEXT}
+
+Answer the following questions based on the given context:
+{QUERY}
+"""
+
+def format_prompt(context, query):
+    prompt = ChatPromptTemplate.from_template(PROMPT)
+    prompt = prompt.format(CONTEXT=context, QUERY=query)
+    return prompt
+    
 if __name__ == "__main__":
+    
+    model = ChatOpenAI(model="gpt-4o-mini")   
+    
     documents = load_PDF("documents")
     chunks = create_chunks(documents, chunk_size=500, chunk_overlap=100)
-    save_chunks_to_database(chunks, "database")
+    vector_db = save_chunks_to_database(chunks, "database")
+    query = 'what is this paper about? what are the main contributions?'
+    context = get_context_from_db(vector_db, query) 
+    prompt = format_prompt(context, query)
+    ans = model.invoke(prompt)
+    print(ans)
